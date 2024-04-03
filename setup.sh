0705dfccd3f00ebb88609343a9af81440234e6e2
Reshma Ramaiah <99098898+ReshmaRamaiah10@users.noreply.github.com>
2024-04-03 19:25:37 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-conda env create -f environment.yml
-conda activate multiome_winner
-conda install -c conda-forge gcc gsl
-pip install MACS2 PhenoGraph doubletdetection SEACells
-Rscript install_packages.R
